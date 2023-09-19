bad241e9eb3e5e829d36a175063fab5d1987db1f
djalma <djalmahy@hotmail.com>
2023-09-19 20:46:39 -0300

    Feat: Spell table css responsivity

--- src/components/spell_table.jsx
+++ src/components/spell_table.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import FirstLevelSpells from "../data/first_level_spells_data";
 import SecondLevelSpells from "../data/second_level_spells_data";
 import ThirdLevelSpells from "../data/third_level_spells_data";
+import "../styles/table.css";
+import "../styles/table_tabs.css";
 import "../styles/spell_table.css";
 
 function SpellTable() {
@@ -42,24 +44,24 @@ function SpellTable() {
         }
 
         return (
-            <table className='content-table'>
+            <table className="content-table">
                 <thead>
                     <tr>
                         <th>Spell Name</th>
                         <th>School</th>
-                        <th>Description</th>
-                        <th>Spell List</th>
+                        <th className="spell-table-head-third">Description</th>
+                        <th className="spell-table-head-fourth">Spell List</th>
                     </tr>
                 </thead>
                 <tbody>
                     {spells.map((spell, index) => (
                         <tr key={index}>
-                            <td>
+                            <td className="spell-table-body-first">
                                 <a onClick={() => handleSpellClick(spell)}>{spell.name}</a>
                             </td>
-                            <td>{spell.type}</td>
-                            <td>{spell.description}</td>
-                            <td>{spell.available}</td>
+                            <td className="spell-table-body-second">{spell.type}</td>
+                            <td className="spell-table-body-third">{spell.description}</td>
+                            <td className="spell-table-body-fourth">{spell.available}</td>
                         </tr>
                     ))}
                 </tbody>
@@ -68,10 +70,10 @@ function SpellTable() {
     };
 
     return (
-        <div className='spell-table-content-wrapper'>
-            <button className='table-button' onClick={() => handleChangeLevel('first')}>1st Level</button>
-            <button className='table-button' onClick={() => handleChangeLevel('second')}>2nd Level</button>
-            <button className='table-button' onClick={() => handleChangeLevel('third')}>3rd Level</button>
+        <div className="spell-table-content-wrapper">
+            <button className="table-tab" onClick={() => handleChangeLevel('first')}>1st Level</button>
+            <button className="table-tab" onClick={() => handleChangeLevel('second')}>2nd Level</button>
+            <button className="table-tab" onClick={() => handleChangeLevel('third')}>3rd Level</button>
             {renderTableByLevel()}
         </div>
     );
