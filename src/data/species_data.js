7b7e3d0fcc5b770d41dd31c7c6ecf65e2d2ce459
djalma <djalmahy@hotmail.com>
2023-09-23 21:35:03 -0300

    Feat: Improved species tab

--- src/data/species_data.js
+++ src/data/species_data.js
@@ -1,8 +1,103 @@
 const species = [
     {
+        name: "Aarakocra",
+        type: "Medium Humanoid",
+        image: require("../assets/images/species/aarakocra.jpg"),
+    },
+    {
+        name: "Changeling",
+        type: "Small or Medium Humanoid, Fey",
+        image: require("../assets/images/species/changeling.jpg"),
+    },
+    {
+        name: "Dragonborn",
+        type: "Medium Humanoid, Dragon",
+        image: require("../assets/images/species/dragonborn.jpg"),
+    },
+    {
+        name: "Dwarf",
+        type: "Medium Humanoid",
+        image: require("../assets/images/species/dwarf.jpg"),
+    },
+    {
+        name: "Dwelve",
+        type: "Medium Humanoid, Fey",
+        image: require("../assets/images/species/dwelve.jpg"),
+    },
+    {
+        name: "Elf",
+        type: "Medium Humanoid, Fey",
+        image: require("../assets/images/species/elf.jpg"),
+    },
+    {
+        name: "Firbolg",
+        type: "Large Humanoid, Fey, Giant",
+        image: require("../assets/images/species/firbolg.png"),
+    },
+    {
+        name: "Gnome",
+        type: "Small Humanoid",
+        image: require("../assets/images/species/gnome.jpg"),
+    },
+    {
+        name: "Goblin",
+        type: "Small Humanoid",
+        image: require("../assets/images/species/goblin.jpg"),
+    },
+    {
+        name: "Grung",
+        type: "Small Humanoid",
+        image: require("../assets/images/species/grung.jpg"),
+    },
+    {
+        name: "Half-Dragon",
+        type: "Medium Humanoid, Dragon",
+        image: require("../assets/images/species/half_dragon.jpg"),
+    },
+    {
+        name: "Half-Orc",
+        type: "Medium Humanoid",
+        image: require("../assets/images/species/half_orc.jpg"),
+    },
+    {
+        name: "Hafling",
+        type: "Small Humanoid",
+        image: require("../assets/images/species/halfling.jpg"),
+    },
+    {
+        name: "Harengon",
+        type: "Small Humanoid",
+        image: require("../assets/images/species/harengon.jpg"),
+    },
+    {
         name: "Human",
         type: "Medium Humanoid",
         image: require("../assets/images/species/human.jpg"),
+    },
+    {
+        name: "Kenku",
+        type: "Medium Humanoid",
+        image: require("../assets/images/species/kenku.jpg"),
+    },
+    {
+        name: "Orc",
+        type: "Medium Humanoid",
+        image: require("../assets/images/species/orc.jpg"),
+    },
+    {
+        name: "Shifter",
+        type: "Medium Humanoid, Monstrosity",
+        image: require("../assets/images/species/shifter.jpg"),
+    },
+    {
+        name: "Tiefling",
+        type: "Medium Humanoid, Fiend",
+        image: require("../assets/images/species/tiefling.jpg"),
+    },
+    {
+        name: "Warforged",
+        type: "Medium Humanoid, Construct",
+        image: require("../assets/images/species/warforged.jpg"),
     },
 ];
   
