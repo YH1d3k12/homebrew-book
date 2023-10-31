5657c92be0c5a269d5f6a5bb7a4d6955199a31c5
djalma <djalmahy@hotmail.com>
2023-10-31 17:44:26 -0300

    Refactor: SpellTable

--- src/pages/SpellBook.jsx
+++ src/pages/SpellBook.jsx
@@ -1,11 +1,31 @@
+import { useEffect,  useState } from 'react';
+
+import TableTabs from '../components/tab/TableTab.jsx';
 import SpellTable from "../features/spellBook/components/SpellTable.jsx";
+
+import data from "../data/spells_data.js";
 import "../features/spellBook/spellBook.css";
 
 
 const SpellBook = () => {
+    const [spells, setSpells] = useState([]);
+    const [currentLevel, setCurrentLevel] = useState(1);
+    
+    // Quando o componente é carregado useEffect é chamado.
+    useEffect(() => {
+        const loadSpells = async () => {
+            setSpells(data)
+        };
+
+        loadSpells();
+    }, []);
+
     return (
         <div className="spell-book-content-wrapper">
-            <SpellTable></SpellTable>
+            <div className="spell-table-container">
+                <TableTabs setCurrentLevel={setCurrentLevel}></TableTabs>
+                <SpellTable data={spells} currentLevel={currentLevel}></SpellTable>
+            </div>
         </div>
     );
 };
