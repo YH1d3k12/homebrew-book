78e3152b49899df9c07d05cd9577aeea732ca4de
Djalma Hideki Yamamoto <69691272+YH1d3k12@users.noreply.github.com>
2023-12-14 13:51:05 -0300

    added data

--- src/data/ArmorsData.js
+++ src/data/ArmorsData.js
@@ -10,7 +10,31 @@ const armorsData = [
         armorClass: "11 + Dex modifier",
         strength: "-",
         stealth: "Disadvantage"
-    }
+    },
+    {
+        name: "Leather Armor",
+        description: "-",
+        cost: "5gp",
+        magical: false,
+        attunement: false,
+        weight: "2.6kg",
+        type: "Light Armor",
+        armorClass: "11 + Dex modifier",
+        strength: "-",
+        stealth: "-"
+    },
+    {
+        name: "Reinforced Leather Armor",
+        description: "-",
+        cost: "9gp",
+        magical: false,
+        attunement: false,
+        weight: "3.1kg",
+        type: "Light Armor",
+        armorClass: "12 + Dex modifier",
+        strength: "-",
+        stealth: "Disadvantage"
+    },
 ];
 
 export default armorsData;
