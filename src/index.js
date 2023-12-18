6782d1245a0c215aa725dd60e2da7fc1be24cad2
Djalma Hideki Yamamoto <69691272+YH1d3k12@users.noreply.github.com>
2023-12-18 11:16:13 -0300

    Refactor: Root colors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/index.css';
+import "./styles/global.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
