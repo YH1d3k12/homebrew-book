f2fa5160afdfff5459bd5c2a4c8186c1aaa2e1bd
djalma <djalmahy@hotmail.com>
2023-12-31 10:59:16 -0300

    Fix: Spell page

--- src/pages/SpellPage.jsx
+++ src/pages/SpellPage.jsx
@@ -1,7 +1,6 @@
 import { useLocation } from "react-router-dom";
 
 import LineBreaker from "../utilities/LineBreaker";
-
 import "../features/spellPage/spellPage.css";
 
 
@@ -11,19 +10,22 @@ const SpellPage = () => {
     
     return (
         <div className="spell-page-content-wrapper">
-            <h1>{spell.name}</h1>
-            <div className="spell-card-content scroll-y">
-                <p>
-                    <strong>Source: </strong>{spell.source}<br/>
-                    <strong>Type: </strong>{spell.type}<br/>
-                    <strong>Casting Time: </strong>{spell.castingTime}<br/>
-                    <strong>Range: </strong>{spell.range}<br/>
-                    <strong>Components: </strong>{spell.components}<br/>
-                    <strong>Duration: </strong>{spell.duration}<br/>
-                </p>
-                <p>{LineBreaker(spell.effect)}</p>
-                <p><strong>At Higher Levels: </strong>{spell.upCasting}</p>
-                <p><strong>Spell List: </strong>{spell.available}</p>
+            <div className="spell-card">
+                <h1>{spell.name}</h1>
+                <div className="spell-card-content">
+                    <h2><strong>Spell: </strong>{spell.enchantment}</h2>
+                    <p>
+                        <strong>Source: </strong>{spell.source}<br/>
+                        <strong>Type: </strong>{spell.type}<br/>
+                        <strong>Casting Time: </strong>{spell.castingTime}<br/>
+                        <strong>Range: </strong>{spell.range}<br/>
+                        <strong>Components: </strong>{spell.components}<br/>
+                        <strong>Duration: </strong>{spell.duration}<br/>
+                    </p>
+                    <p>{LineBreaker(spell.effect)}</p>
+                    <p><strong>At Higher Levels: </strong>{spell.upCasting}</p>
+                    <p><strong>Spell List: </strong>{spell.available}</p>
+                </div>
             </div>
         </div>
     );
