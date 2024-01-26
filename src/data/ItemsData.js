901d5d803439417168783ffdc481e4f104df34fb
Djalma Hideki Yamamoto <69691272+YH1d3k12@users.noreply.github.com>
2024-01-26 11:16:44 -0300

    Feat: item page render specific properties

--- src/data/ItemsData.js
+++ src/data/ItemsData.js
@@ -7,7 +7,7 @@ const ItemsData = [
         description: "Dwarf Crack, also known as Red Sugar, is a glowing, deep-red crystalline resource found in the deeps. Dwarf Crack is an edible sweet rock with healing and addictive properties, as noted by the Dwarves.\n Consuming raw Red Sugar restores 1d4 hit points; if the character is a dwarf, it restores the maximum value. It is observed that healing potions made of Red Sugar also achieve maximum efficiency on dwarves.",
         cost: "15gp",
         type: "",
-        properties: {
+        effects: {
             "Restore Health": 3,
             "Restore Stamina": 2,
             "Fortify Speed": 2
@@ -21,7 +21,7 @@ const ItemsData = [
         description: "",
         cost: "20gp",
         type: "",
-        properties: {
+        effects: {
             "Regenerate Health": 2,
             "Fortify Health": 2,
             "Weakness Acid": 2,
