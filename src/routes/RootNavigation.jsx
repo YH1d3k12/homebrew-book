38ae72b527c15cc1d226d9264bfabc994418d56c
Djalma Hideki Yamamoto <69691272+YH1d3k12@users.noreply.github.com>
2024-02-02 10:22:28 -0300

    Refactor: Renamed species to speciebook

--- src/routes/RootNavigation.jsx
+++ src/routes/RootNavigation.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Body from "../pages/Body.jsx";
 import Menu from "../pages/Menu.jsx";
-import Species from "../pages/Species.jsx";
+import SpecieBook from "../pages/SpecieBook.jsx";
 import SpellBook  from "../pages/SpellBook.jsx";
 import SpellPage  from "../pages/SpellPage.jsx";
 import TechniqueBook from "../pages/TechniqueBook.jsx";
@@ -19,7 +19,7 @@ export default function RootNavigation()
 			<Routes>
 				<Route path="/" element={<Body  />}>
 					<Route path="/" element={<Menu  />} />
-					<Route path="/species" element={<Species  />} />
+					<Route path="/species" element={<SpecieBook  />} />
 					<Route path="/spellbook" element={<SpellBook  />} />
 					<Route path="/spellpage" element={<SpellPage  />} />
 					<Route path="/techniquebook" element={<TechniqueBook  />} />
